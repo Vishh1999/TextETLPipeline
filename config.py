d852c6e34ad5db76561ae866465a7f290b64d916
vishak <lvvishak@gmail.com>
2026-01-12 21:39:37 +0000

    sample config file

---
+++ config.py
@@ -0,0 +1,20 @@
+# Important Variables
+DATA_PATH = "your_data_path"
+
+MONGO_DB_NAME="your_mongo_db_name"
+
+MONGO_COLLECTION_NAME="your_mongo_collection_name"
+
+MONGO_URI="mongodb://localhost:27017/"
+
+BATCH_SIZE=500
+
+POSTGRES_DB_CONFIG = {
+    "host": "localhost",
+    "port": 5432,
+    "dbname": "your_db_name",
+    "user": "your_db_user",
+}
+POSTGRES_MAIN_TABLE_NAME="your_postgresql_main_table_name"
+
+POSTGRES_ANALYTICS_TABLE_NAME="your_postgresql_analytics_table_name"
